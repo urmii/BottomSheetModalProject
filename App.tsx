8254820ad202511a7192e07aa3265bae21c50733
Urmi Soni <urmimahajan2014@gmail.com>
2024-07-22 19:22:51 +0100

    button changes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import React, { useRef, useState, useMemo } from 'react';
-import { SafeAreaView, StyleSheet, View, Text, TextInput, TouchableOpacity, FlatList, Keyboard, TouchableWithoutFeedback, Platform, KeyboardAvoidingView, useWindowDimensions ,Linking } from 'react-native';
+import {
+  SafeAreaView, StyleSheet, View, Text, TextInput, TouchableOpacity, FlatList, Keyboard,
+  TouchableWithoutFeedback, Platform, KeyboardAvoidingView, Linking
+} from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
@@ -8,7 +11,7 @@ const App = () => {
   const [inputText, setInputText] = useState('');
   const [submittedTexts, setSubmittedTexts] = useState<string[]>([]);
   const [error, setError] = useState('');
-  const snapPoints = useMemo(() => ["65%"], []);
+  const snapPoints = useMemo(() => ["50%"], []);
   const [showClearButton, setShowClearButton] = useState(false); 
   const handleEmailPress = () => {
       Linking.openURL('mailto:support@example.com');
@@ -46,25 +49,36 @@ const App = () => {
     <GestureHandlerRootView style={styles.container}>
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
-          <Text style={styles.title}>Welcome to Bottom Sheet Modal Project! Easily manage and view your keynotes.</Text>
-          <Text style={styles.instructions}>1. Tap 'Open Bottom Sheet' to add new keynotes.</Text>
-          <Text style={styles.instructions}>2. Save your notes to have them listed here.</Text>
+          <Text style={styles.title}>Welcome to keypoints!</Text>
         </View>
+
+        <View style={styles.bottomSheetButtons}>
+        <TouchableOpacity style={[styles.bottomSheetButton, styles.openBottomSheetButton]} onPress={() => bottomSheetRef.current?.expand()}>
+          <Text style={styles.openBottomSheetButtonText}>Add keypoint</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.bottomSheetButton, styles.cancelButton]} onPress={handleCancel}>
+          <Text style={styles.cancelbuttonText}>Close</Text>
+        </TouchableOpacity>
+        </View>
+
+        <View style={styles.header}>
+          <Text style={styles.instructions}>1. Tap 'Add Keypoints' to add new keypoints.</Text>
+        <Text style={styles.instructions}>2. Save your notes to have them listed here.</Text>
+        </View>
+
         <FlatList
           data={submittedTexts}
           keyExtractor={(item, index) => index.toString()}
           renderItem={({ item }) => <Text style={styles.bulletItem}>{`\u2022 ${item}`}</Text>}
           style={styles.bulletList}
         />
+
         {showClearButton && (
           <TouchableOpacity style={styles.clearButton} onPress={handleClear}>
-          <Text style={styles.clearButtonText}>Clear All</Text>
+              <Text style={styles.clearButtonText}>Clear All</Text>
           </TouchableOpacity>
         )}
-        <TouchableOpacity style={styles.openBottomSheetButton} onPress={() => bottomSheetRef.current?.expand()}>
-          <Text style={styles.openBottomSheetButtonText}>Add Keynote</Text>
-        </TouchableOpacity>
-      
+        
         <BottomSheet
           ref={bottomSheetRef}
           index={-1} 
@@ -79,16 +93,13 @@ const App = () => {
               <View style={styles.bottomSheetContent}>
                 <TextInput
                   style={[styles.input, error ? styles.inputError : null]}
-                  placeholder="Keynotes"
+                  placeholder="keypoints"
                   value={inputText}
                   onChangeText={setInputText}
                   placeholderTextColor="#888"
                 />
                 {error ? <Text style={styles.errorText}>{error}</Text> : null}
                 <View style={styles.bottomSheetButtons}>
-                  <TouchableOpacity style={[styles.bottomSheetButton, styles.cancelButton]} onPress={handleCancel}>
-                    <Text style={styles.cancelbuttonText}>Close</Text>
-                  </TouchableOpacity>
                   <TouchableOpacity style={[styles.bottomSheetButton, styles.saveButton]} onPress={handleSubmit}>
                     <Text style={styles.savebuttonText}>Save</Text>
                   </TouchableOpacity>
@@ -117,7 +128,6 @@ const styles = StyleSheet.create({
     padding: 15,
   },
   header: {
-    marginBottom: 20,
     alignItems: 'center',
   },
   title: {
@@ -126,6 +136,25 @@ const styles = StyleSheet.create({
     color: '#fff',
     marginBottom: 10,
     marginTop:30,
+  },
+  openBottomSheetButtonText: {
+    color: '#4b666b',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  cancelButton: {
+    borderRadius: 10,
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
+  cancelbuttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold', 
+  },
+  bottomSheetBackground: {
+    backgroundColor: '#c7edd0', 
   },
   instructions: {
     fontSize: 16,
@@ -136,8 +165,8 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   bulletList: {
-    marginTop: 20,
-    paddingHorizontal: 10,
+    marginTop: 25,
+    paddingHorizontal: 15,
   },
   bulletItem: {
     fontSize: 18,
@@ -145,23 +174,21 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     paddingVertical: 5,
   },
+  bottomSheetButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bottomSheetButton: {
+    flex: 1,
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
   openBottomSheetButton: {
     backgroundColor: '#83cfd6',
-    padding: 15,
-    alignItems: 'center',
+    padding: 10,
     borderRadius: 10,
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-  },
-  openBottomSheetButtonText: {
-    color: '#4b666b',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  bottomSheetBackground: {
-    backgroundColor: '#c7edd0',
-    
+    marginRight:25,
   },
   handleIndicator: {
     backgroundColor: '#ccc',
@@ -194,34 +221,11 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     textAlign: 'center',
   },
-  bottomSheetButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  bottomSheetButton: {
-    flex: 1,
-    padding: 10,
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-  cancelButton: {
-    marginRight: 5,
-    borderRadius: 10,
-    backgroundColor: 'transparent',
-    borderWidth: 1,
-    borderColor: '#4b666b',
-  },
-  cancelbuttonText: {
-    color: '#4b666b',
-    fontSize: 16,
-    fontWeight: 'bold', 
-  },
   saveButton: {
     backgroundColor: '#83cfd6',
-    marginLeft: 5,
   },
   savebuttonText: {
-    color: 'white',
+    color: '#4b666b',
     fontSize: 16,
     fontWeight: 'bold',
   },
@@ -236,7 +240,7 @@ const styles = StyleSheet.create({
     padding: 15,
     alignItems: 'center',
     position: 'absolute',
-    bottom: 70, 
+    bottom: 10, 
     width: '100%',
     color: '#4b666b',
     fontSize: 16,
